f0f1c12dea75e12f3e5a93b14b52e7726cf50337
Juanite <juan13_barsa@hotmail.com>
2025-02-07 18:48:43 +0100

    Ejercicio 5

---
+++ src/lib/actions.js
@@ -0,0 +1,191 @@
+'use server'
+import { revalidatePath } from 'next/cache'
+import prisma from '@/lib/prisma'
+import { obtenerPedidos } from './data'
+
+
+//  ------------------------ REPARTIDORES ------------------------
+
+
+export async function insertarRepartidor(formData) {
+    const nombre = formData.get('nombre')
+    const telefono = formData.get('telefono')
+
+    await prisma.repartidor.create({
+        data: {
+            nombre: nombre,
+            telefono: telefono,
+        }
+    })
+
+    revalidatePath('/repartidores')
+
+}
+
+
+
+export async function modificarRepartidor(formData) {
+    const id = Number(formData.get('id'))
+    const nombre = formData.get('nombre')
+    const telefono = formData.get('telefono')
+
+
+    await prisma.repartidor.update({
+        where: {
+            id: id
+        },
+        data: {
+            nombre: nombre,
+            telefono: telefono,
+        }
+    })
+
+    revalidatePath('/repartidores')
+}
+
+
+
+export async function eliminarRepartidor(formData) {
+    const id = Number(formData.get('id'))
+
+    await prisma.repartidor.delete({
+        where: {
+            id: id
+        }
+    })
+
+    revalidatePath('/repartidores')
+
+}
+
+
+//  ------------------------ PEDIDOS ------------------------
+
+
+export async function insertarPedidos(formData) {
+    const nombre = formData.get('nombre')
+    const fecha_y_hora = new Date(formData.get('fecha_y_hora'))
+    const nombre_cliente = formData.get('nombre_cliente')
+    const direccion_cliente = formData.get('direccion_cliente')
+    const repartidor_id = Number(formData.get('repartidor_id'))
+    await prisma.pedido.create({
+        data: {
+            nombre: nombre,
+            fecha_y_hora: fecha_y_hora,
+            nombre_cliente: nombre_cliente,
+            direccion_cliente: direccion_cliente,
+            grupo: {
+                connect: {
+                    id: repartidor_id
+                }
+            },
+        }
+    })
+
+    revalidatePath('/pedidos')
+
+}
+
+
+
+export async function modificarPedido(formData) {
+    const id = Number(formData.get('id'))
+    const nombre = formData.get('nombre')
+    const fecha_y_hora = new Date(formData.get('fecha_y_hora'))
+    const nombre_cliente = formData.get('nombre_cliente')
+    const direccion_cliente = formData.get('direccion_cliente')
+    const repartidor_id = Number(formData.get('repartidor_id'))
+
+    await prisma.pedido.update({
+        where: {
+            id: id
+        },
+        data: {
+            nombre: nombre,
+            fecha_y_hora: fecha_y_hora,
+            nombre_cliente: nombre_cliente,
+            direccion_cliente: direccion_cliente,
+            grupo: {
+                connect: {
+                    id: repartidor_id
+                }
+            },
+        }
+    })
+
+    revalidatePath('/pedidos')
+}
+
+
+
+export async function eliminarPedido(formData) {
+    const id = Number(formData.get('id'))
+
+    await prisma.pedido.delete({
+        where: {
+            id: id
+        }
+    })
+
+    revalidatePath('/pedidos')
+
+}
+
+// ------------------------------- PIZZAS -----------------------
+
+
+export async function insertarPizza(formData) {
+    const nombre = formData.get('nombre')
+    const precio = formData.get('precio')
+    const pedidoIds = formData.getAll("pedidos");
+
+    await prisma.pizza.create({
+        data: {
+            nombre: nombre,
+            precio: precio,
+            pedidos: {
+                connect: pedidoIds.map(id => ({ id: Number(id) }))
+            }
+        }
+        
+    })
+
+    revalidatePath('/pizzas')
+
+}
+
+
+
+export async function modificarPizza(formData) {
+    const id = Number(formData.get('id'))
+    const nombre = formData.get('nombre')
+    const precio = formData.get('precio')
+
+
+    await prisma.pizza.update({
+        where: {
+            id: id
+        },
+        data: {
+            nombre: nombre,
+            precio: precio,
+        }
+    })
+
+    revalidatePath('/pizzas')
+}
+
+
+
+export async function eliminarPizza(formData) {
+    const id = Number(formData.get('id'))
+
+    await prisma.pizza.delete({
+        where: {
+            id: id
+        }
+    })
+
+    revalidatePath('/pizzas')
+
+}
